3f59e8815297ea4d5ca8ccfa97253319ffc17709
ppparkta <pppppark2@gmail.com>
2024-09-18 19:54:07 +0900

    test: 기분목록조회 레포지토리 단위 테스트(조회) (#40)

--- server/src/test/java/com/exchangediary/global/domain/StaticImageRepositoryTest.java
+++ server/src/test/java/com/exchangediary/global/domain/StaticImageRepositoryTest.java
@@ -2,20 +2,18 @@ package com.exchangediary.global.domain;
 
 import com.exchangediary.global.domain.entity.StaticImage;
 import com.exchangediary.global.domain.entity.StaticImageType;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class StaticImageRepositoryTest {
     @Autowired
     private StaticImageRepository staticImageRepository;
-
-    @Test
-    public void 레포지토리가null이아님() {
-        Assertions.assertThat(staticImageRepository).isNotNull();
-    }
 
     @Test
     public void 기분이미지등록() {
@@ -24,12 +22,32 @@ class StaticImageRepositoryTest {
                 .type(StaticImageType.MOOD)
                 .url("moodImageUrl")
                 .build();
+
         //when
         final StaticImage newImage = staticImageRepository.save(image);
 
         //then
-        Assertions.assertThat(newImage.getId()).isNotNull();
-        Assertions.assertThat(newImage.getType()).isEqualTo(StaticImageType.MOOD);
-        Assertions.assertThat(newImage.getUrl()).isEqualTo("moodImageUrl");
+        assertThat(newImage.getId()).isNotNull();
+        assertThat(newImage.getType()).isEqualTo(StaticImageType.MOOD);
+        assertThat(newImage.getUrl()).isEqualTo("moodImageUrl");
+    }
+
+    @Test
+    public void 기분이미지조회() {
+        //given
+        final StaticImage image = StaticImage.builder()
+                .type(StaticImageType.MOOD)
+                .url("moodImageUrl")
+                .build();
+
+        //when
+        staticImageRepository.save(image);
+        List<StaticImage> findResult = staticImageRepository.findAllByType(StaticImageType.MOOD);
+
+        //then
+        assertThat(findResult).isNotNull();
+        assertThat(findResult.get(0).getId()).isNotNull();
+        assertThat(findResult.get(0).getType()).isEqualTo(StaticImageType.MOOD);
+        assertThat(findResult.get(0).getUrl()).isEqualTo("moodImageUrl");
     }
 }
