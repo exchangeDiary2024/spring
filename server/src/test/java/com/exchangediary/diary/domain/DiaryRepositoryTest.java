4b2d2d9c51ab9ca67560756efd672d6de81fd0ce
jisunggi <wltjd5316@naver.com>
2024-09-19 19:03:54 +0900

    feat: diaryrepositorytest 구현 (#49)

---
+++ server/src/test/java/com/exchangediary/diary/domain/DiaryRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.exchangediary.diary.domain;
+
+import com.exchangediary.diary.domain.entity.Diary;
+import com.exchangediary.diary.domain.entity.UploadImage;
+import com.exchangediary.global.domain.entity.StaticImage;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@DataJpaTest
+public class DiaryRepositoryTest {
+    @Autowired
+    private DiaryRepository diaryRepository;
+
+    @Test
+    public void testSaveDiary() {
+        //given
+        StaticImage moodImage = mock(StaticImage.class);
+        UploadImage uploadImage = mock(UploadImage.class);
+        Diary newDiary = diary(moodImage, uploadImage);
+
+        //when
+        Diary savedDiary = diaryRepository.save(newDiary);
+
+        //then
+        assertThat(savedDiary).isNotNull();
+        assertThat(savedDiary.getId()).isEqualTo(newDiary.getId());
+        assertThat(savedDiary.getContent()).isEqualTo("내용");
+    }
+
+    private Diary diary(StaticImage moodImage, UploadImage uploadImage) {
+        return Diary.builder()
+                .content("내용")
+                .moodImage(moodImage)
+                .uploadImage(uploadImage)
+                .build();
+    }
+
+
+}
