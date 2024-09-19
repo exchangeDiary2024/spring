0945ba82f4b51f2770b770c750b7274032e8046f
ppparkta <pppppark2@gmail.com>
2024-09-19 10:15:50 +0900

    style: 테스트용 application.yml 이름 구분 및 RepositoryTest에 yml 지정(#40)

--- server/src/test/java/com/exchangediary/global/domain/StaticImageRepositoryTest.java
+++ server/src/test/java/com/exchangediary/global/domain/StaticImageRepositoryTest.java
@@ -5,12 +5,14 @@ import com.exchangediary.global.domain.entity.StaticImageType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.yml")
 class StaticImageRepositoryTest {
     @Autowired
     private StaticImageRepository staticImageRepository;
