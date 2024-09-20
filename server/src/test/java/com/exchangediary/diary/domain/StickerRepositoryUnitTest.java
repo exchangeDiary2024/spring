b99ed9e1877d953125d7ee43d117ccbb9e952203
yeeuniii <anna862700@gmail.com>
2024-09-20 16:43:39 +0900

    test: repository 테스트 데이터베이스 h2로 변경 (#25)

--- server/src/test/java/com/exchangediary/diary/domain/StickerRepositoryUnitTest.java
+++ server/src/test/java/com/exchangediary/diary/domain/StickerRepositoryUnitTest.java
@@ -6,13 +6,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StickerRepositoryUnitTest {
     @PersistenceContext
     private EntityManager entityManager;
