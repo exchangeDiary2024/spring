ba030e0e38a03f410683215c7cd8b214fd7e71b0
yeeuniii <anna862700@gmail.com>
2024-09-20 20:13:02 +0900

    test: sticker repository save 메서드 테스트 추가 (#25)

--- server/src/test/java/com/exchangediary/diary/domain/StickerRepositoryUnitTest.java
+++ server/src/test/java/com/exchangediary/diary/domain/StickerRepositoryUnitTest.java
@@ -2,6 +2,7 @@ package com.exchangediary.diary.domain;
 
 import com.exchangediary.diary.domain.entity.Diary;
 import com.exchangediary.diary.domain.entity.Sticker;
+import com.exchangediary.global.domain.entity.StaticImage;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,36 @@ public class StickerRepositoryUnitTest {
     private EntityManager entityManager;
     @Autowired
     private StickerRepository stickerRepository;
+
+    @Test
+    void 스티커_저장() {
+        StaticImage staticImage = StaticImage.builder().build();
+        Diary diary = Diary.builder().build();
+        entityManager.persist(staticImage);
+        entityManager.persist(diary);
+        Sticker sticker = Sticker.builder()
+                .Id(1L)
+                .coordX(10.0)
+                .coordY(20.0)
+                .coordZ(1)
+                .height(200.0)
+                .width(100.0)
+                .rotation(90.0)
+                .staticImage(staticImage)
+                .diary(diary)
+                .build();
+
+        Sticker saved = stickerRepository.save(sticker);
+
+        assertThat(saved.getCoordX()).isEqualTo(sticker.getCoordX());
+        assertThat(saved.getCoordY()).isEqualTo(sticker.getCoordY());
+        assertThat(saved.getCoordZ()).isEqualTo(sticker.getCoordZ());
+        assertThat(saved.getHeight()).isEqualTo(sticker.getHeight());
+        assertThat(saved.getWidth()).isEqualTo(sticker.getWidth());
+        assertThat(saved.getRotation()).isEqualTo(sticker.getRotation());
+        assertThat(saved.getStaticImage().getId()).isEqualTo(sticker.getStaticImage().getId());
+        assertThat(saved.getDiary().getId()).isEqualTo(sticker.getDiary().getId());
+    }
 
     @Test
     void 일기에_붙여진_스티커_개수() {
