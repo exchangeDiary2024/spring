71b89808e806508b2cdfa32fd5e29839301c930d
parkchu <parkjoohan0603@gmail.com>
2024-10-17 18:44:39 +0900

    feat: 그룹 상세 정보 페이지 기본틀 구현 (#135)

--- src/main/resources/static/js/group/group-menu.js
+++ src/main/resources/static/js/group/group-menu.js
@@ -1,9 +1,11 @@
 const menuBtn = document.querySelector(".menu-btn");
 const groupMenu = document.querySelector(".group-menu");
 const menu = groupMenu.querySelector(".menu");
+const groupMembers = menu.querySelector(".group-members");
 
 menuBtn.addEventListener("click", openMenu);
 groupMenu.addEventListener("click", closeMenu);
+drawMembers(3);
 
 function openMenu() {
     groupMenu.style.display = "block";
@@ -17,4 +19,32 @@ function closeMenu(event) {
         groupMenu.classList.remove("blur");
         setTimeout(() => groupMenu.style.display = "none", 300);
     }
+}
+
+function drawMembers(memberSize) {
+    const centerX = 88;
+    const centerY = 119;
+    const r = 79;
+
+    for (number = 0; number < memberSize; number++) {
+        const groupMember = document.createElement("div");
+        const radian = getRadian(getAngle(number, memberSize))
+        groupMember.classList.add("group-member");
+        groupMember.innerText = number + 1;
+        groupMember.style.left = `${centerX - r * Math.cos(radian)}px`;
+        groupMember.style.top = `${centerY - r * Math.sin(radian)}px`;
+        groupMembers.appendChild(groupMember);
+    }
+}
+
+function getRadian(angle) {
+    return angle * Math.PI / 180;
+}
+
+function getAngle(number, memberSize) {
+    const angle = 270 / (memberSize - 1) * number;
+    if (isFinite(angle)) {
+        return angle;
+    }
+    return 0;
 }
