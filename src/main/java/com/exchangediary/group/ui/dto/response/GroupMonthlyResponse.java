ac0564b7f1b238f88dbc1f33b161229b6ac9cb50
yeeuniii <anna862700@gmail.com>
2024-10-19 02:22:18 +0900

    refactor: 그룹 생성 년월 정보 형식 수정 (#136)

--- src/main/java/com/exchangediary/group/ui/dto/response/GroupMonthlyResponse.java
+++ src/main/java/com/exchangediary/group/ui/dto/response/GroupMonthlyResponse.java
@@ -3,19 +3,19 @@ package com.exchangediary.group.ui.dto.response;
 import com.exchangediary.group.domain.entity.Group;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
-
 @Builder
 public record GroupMonthlyResponse(
         Long id,
         String name,
-        LocalDateTime createdAt
+        int createdYear,
+        int createdMonth
 ) {
     public static GroupMonthlyResponse of(Group group) {
         return GroupMonthlyResponse.builder()
                 .id(group.getId())
                 .name(group.getName())
-                .createdAt(group.getCreatedAt())
+                .createdYear(group.getCreatedAt().getYear())
+                .createdMonth(group.getCreatedAt().getMonthValue())
                 .build();
     }
 }
