ef6ee812b98b50915b0bac4e94fe973a3b0e20be
yeeuniii <anna862700@gmail.com>
2024-10-18 16:34:14 +0900

    feat: 그룹원 순서에 따른 정보 조회 api 컨트롤러 추가 (#268)

--- src/main/java/com/exchangediary/group/ui/dto/response/GroupMembersResponse.java
+++ src/main/java/com/exchangediary/group/ui/dto/response/GroupMembersResponse.java
@@ -7,24 +7,24 @@ import java.util.List;
 
 @Builder
 public record GroupMembersResponse(
-        List<MemberResponse> members
+        List<GroupMemberResponse> members
 ) {
     public static GroupMembersResponse from(List<Member> members) {
-        List<MemberResponse> memberResponses = members.stream()
-                .map(MemberResponse::from)
+        List<GroupMemberResponse> response = members.stream()
+                .map(GroupMemberResponse::from)
                 .toList();
         return GroupMembersResponse.builder()
-                .members(memberResponses)
+                .members(response)
                 .build();
     }
 
     @Builder
-    private record MemberResponse(
+    public record GroupMemberResponse(
             String nickname,
             String profileImage
     ) {
-        public static MemberResponse from(Member member) {
-            return MemberResponse.builder()
+        public static GroupMemberResponse from(Member member) {
+            return GroupMemberResponse.builder()
                     .nickname(member.getNickname())
                     .profileImage(member.getProfileImage())
                     .build();
