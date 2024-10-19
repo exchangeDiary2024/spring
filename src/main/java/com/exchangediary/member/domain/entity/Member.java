694998aa893865c7b1b7471a5b8f769dfda48e21
yeeuniii <anna862700@gmail.com>
2024-10-20 03:03:18 +0900

    feat: group role enum 생성 및 멤버 엔티티에 그룹 역할 필드 추가 (#237)

--- src/main/java/com/exchangediary/member/domain/entity/Member.java
+++ src/main/java/com/exchangediary/member/domain/entity/Member.java
@@ -2,8 +2,11 @@ package com.exchangediary.member.domain.entity;
 
 import com.exchangediary.global.domain.entity.BaseEntity;
 import com.exchangediary.group.domain.entity.Group;
+import com.exchangediary.member.domain.enums.GroupRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
@@ -35,14 +38,23 @@ public class Member extends BaseEntity {
     private String nickname;
     private String profileImage;
     private Integer orderInGroup;
+    @Enumerated(EnumType.STRING)
+    private GroupRole groupRole;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id", foreignKey = @ForeignKey(name = "member_group_id_fkey"))
     private Group group;
 
-    public void updateMemberGroupInfo(String nickname, String profileImage, int orderInGroup, Group group) {
+    public void updateMemberGroupInfo(
+            String nickname,
+            String profileImage,
+            int orderInGroup,
+            GroupRole groupRole,
+            Group group
+    ) {
         this.nickname = nickname;
         this.profileImage = profileImage;
         this.orderInGroup = orderInGroup;
+        this.groupRole = groupRole;
         this.group = group;
     }
 }
