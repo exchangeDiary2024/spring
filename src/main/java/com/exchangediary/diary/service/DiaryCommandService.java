47afcb76d96d5abddd3a2cb84ba0805f719138db
yeeuniii <anna862700@gmail.com>
2024-10-23 01:43:41 +0900

    refactor: 일기 유효성 검사 서비스 분리 (#284)

--- src/main/java/com/exchangediary/diary/service/DiaryCommandService.java
+++ src/main/java/com/exchangediary/diary/service/DiaryCommandService.java
@@ -5,7 +5,6 @@ import com.exchangediary.diary.domain.entity.Diary;
 import com.exchangediary.diary.domain.entity.UploadImage;
 import com.exchangediary.diary.ui.dto.request.DiaryRequest;
 import com.exchangediary.global.exception.ErrorCode;
-import com.exchangediary.global.exception.serviceexception.DuplicateException;
 import com.exchangediary.global.exception.serviceexception.FailedImageUploadException;
 import com.exchangediary.global.exception.serviceexception.ForbiddenException;
 import com.exchangediary.group.domain.GroupRepository;
@@ -19,30 +18,22 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.Optional;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class DiaryCommandService {
-    private static final Set<String> VALID_IMAGE_FORMAT = Set.of(
-            "image/jpeg",
-            "image/gif",
-            "image/png",
-            "image/heic",
-            "image/heif"
-    );
-    private final DiaryRepository diaryRepository;
+
     private final MemberQueryService memberQueryService;
     private final GroupQueryService groupQueryService;
+    private final DiaryValidationService diaryValidationService;
+    private final DiaryRepository diaryRepository;
     private final GroupRepository groupRepository;
 
     public Long createDiary(DiaryRequest diaryRequest, MultipartFile file, Long groupId, Long memberId) {
         Member member = memberQueryService.findMember(memberId);
         Group group = groupQueryService.findGroup(groupId);
-        checkTodayDiaryExistent(groupId);
+        diaryValidationService.checkTodayDiaryExistent(groupId);
         //Todo: 일기 작성 인가 후 삭제
         if (!member.getOrderInGroup().equals(group.getCurrentOrder())) {
             throw new ForbiddenException(
@@ -60,7 +51,7 @@ public class DiaryCommandService {
             return savedDiary.getId();
         }
 
-        validateImageType(file);
+        diaryValidationService.validateImageType(file);
         try {
             UploadImage image = UploadImage.builder()
                     .image(file.getBytes())
@@ -80,24 +71,6 @@ public class DiaryCommandService {
         }
     }
 
-    private void checkTodayDiaryExistent(Long groupId) {
-        LocalDate today = LocalDate.now();
-        Optional<Long> todayDiary =
-                diaryRepository.findIdByGroupAndDate(
-                        groupId,
-                        today.getYear(),
-                        today.getMonthValue(),
-                        today.getDayOfMonth());
-
-        if (todayDiary.isPresent()) {
-            throw new DuplicateException(
-                    ErrorCode.DIARY_DUPLICATED,
-                    "",
-                    today.toString()
-            );
-        }
-    }
-
     private boolean isEmptyFile(MultipartFile file) {
         return file == null || file.isEmpty();
     }
@@ -108,17 +81,5 @@ public class DiaryCommandService {
             currentOrder = 1;
         group.updateCurrentOrder(currentOrder);
         groupRepository.save(group);
-    }
-
-    private void validateImageType(MultipartFile file) {
-        String contentType = file.getContentType();
-
-        if (!VALID_IMAGE_FORMAT.contains(contentType)) {
-            throw new FailedImageUploadException(
-                    ErrorCode.INVALID_IMAGE_FORMAT,
-                    "",
-                    file.getOriginalFilename()
-            );
-        }
     }
 }
