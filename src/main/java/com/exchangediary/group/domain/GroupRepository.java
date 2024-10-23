3b5b910101c820f02d1d3b8bc8e6798ce94fd2b0
yeeuniii <anna862700@gmail.com>
2024-10-24 01:56:13 +0900

    fix: 사용자 그룹순서와 현재 그룹순서 비교 쿼리문 수정 (#253)

--- src/main/java/com/exchangediary/group/domain/GroupRepository.java
+++ src/main/java/com/exchangediary/group/domain/GroupRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findByCode(String code);
-    @Query("SELECT g.id FROM Group g JOIN g.members m WHERE m.id = :memberId")
+    @Query("SELECT g.id FROM Group g JOIN g.members m ON m.id = :memberId WHERE m.orderInGroup = g.currentOrder")
     Optional<Long> findGroupIdCurrentOrderEqualsMemberOrder(Long memberId);
 }
