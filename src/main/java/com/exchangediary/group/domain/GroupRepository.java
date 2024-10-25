61c05f2405e01786568f8113b6ded670aadb9fa4
yeeuniii <anna862700@gmail.com>
2024-10-25 14:32:18 +0900

    style: 개행 제거 (#295)

--- src/main/java/com/exchangediary/group/domain/GroupRepository.java
+++ src/main/java/com/exchangediary/group/domain/GroupRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findByCode(String code);
     @Query("SELECT CASE WHEN m.orderInGroup = g.currentOrder THEN true ELSE false END " +
-            "FROM Group g JOIN g.members m " +
-            "ON m.id = :memberId")
+            "FROM Group g JOIN g.members m ON m.id = :memberId")
     Boolean isEqualsToGroupCurrentOrder(Long memberId);
 }
