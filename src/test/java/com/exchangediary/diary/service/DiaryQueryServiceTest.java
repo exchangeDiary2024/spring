dde908e71751b446e60e29f8bda08199fea4c38b
yeeuniii <anna862700@gmail.com>
2024-10-25 17:03:25 +0900

    style: 사용하지 않는 import 제거 (#296)

--- src/test/java/com/exchangediary/diary/service/DiaryQueryServiceTest.java
+++ src/test/java/com/exchangediary/diary/service/DiaryQueryServiceTest.java
@@ -7,15 +7,11 @@ import com.exchangediary.diary.ui.dto.response.DiaryResponse;
 import com.exchangediary.global.exception.serviceexception.NotFoundException;
 import com.exchangediary.group.domain.GroupRepository;
 import com.exchangediary.group.domain.entity.Group;
-import com.exchangediary.member.domain.MemberRepository;
 import com.exchangediary.member.domain.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
