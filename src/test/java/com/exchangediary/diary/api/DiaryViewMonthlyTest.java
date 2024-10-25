5e5c30d24b4a1e3d268d32ead33733b1b7af8b90
yeeuniii <anna862700@gmail.com>
2024-10-25 23:24:26 +0900

    test: 테스트 수정 (#294)

--- src/test/java/com/exchangediary/diary/api/DiaryViewMonthlyTest.java
+++ src/test/java/com/exchangediary/diary/api/DiaryViewMonthlyTest.java
@@ -3,17 +3,20 @@ package com.exchangediary.diary.api;
 import com.exchangediary.ApiBaseTest;
 import com.exchangediary.diary.domain.DiaryRepository;
 import com.exchangediary.diary.domain.entity.Diary;
+import com.exchangediary.diary.ui.dto.response.DiaryDayResponse;
 import com.exchangediary.diary.ui.dto.response.DiaryMonthlyResponse;
 import com.exchangediary.global.exception.ErrorCode;
 import com.exchangediary.group.domain.GroupRepository;
 import com.exchangediary.group.domain.entity.Group;
+import com.exchangediary.member.domain.enums.GroupRole;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import java.util.Arrays;
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -26,14 +29,11 @@ class DiaryViewMonthlyTest extends ApiBaseTest {
     private DiaryRepository diaryRepository;
 
     @Test
-    void 달력형_일기_조회_성공() {
+    @DisplayName("사용자가 오늘 일기까지 조회 가능")
+    void 달력형_일기_조회_성공_조회_가능한_일기() {
         Group group = createGroup();
-        Group otherGroup = createGroup();
-        groupRepository.saveAll(Arrays.asList(group, otherGroup));
+        updateSelf(group, LocalDate.now());
         Diary diary = createDiary(group);
-        Diary diary2 = createDiary(group);
-        Diary otherDiary = createDiary(otherGroup);
-        diaryRepository.saveAll(Arrays.asList(diary, diary2, otherDiary));
 
         DiaryMonthlyResponse response = RestAssured
                 .given().log().all()
@@ -46,15 +46,41 @@ class DiaryViewMonthlyTest extends ApiBaseTest {
                 .statusCode(HttpStatus.OK.value())
                 .extract().as(DiaryMonthlyResponse.class);
 
-        assertThat(response.year()).isEqualTo(diary.getCreatedAt().getYear());
-        assertThat(response.month()).isEqualTo(diary.getCreatedAt().getMonth().getValue());
-        assertThat(response.days()).hasSize(2);
+        assertThat(response.days()).hasSize(1);
+        DiaryDayResponse day = response.days().get(0);
+        assertThat(day.canView()).isTrue();
+        assertThat(day.day()).isEqualTo(diary.getCreatedAt().getDayOfMonth());
+        assertThat(day.profileImage()).isEqualTo(this.member.getProfileImage());
+    }
+
+    @Test
+    @DisplayName("사용자가 어제 일기까지 조회 가능")
+    void 달력형_일기_조회_성공_조회_불가능한_일기() {
+        Group group = createGroup();
+        updateSelf(group, LocalDate.now().minusDays(1));
+        Diary diary = createDiary(group);
+
+        DiaryMonthlyResponse response = RestAssured
+                .given().log().all()
+                .queryParam("year", diary.getCreatedAt().getYear())
+                .queryParam("month", diary.getCreatedAt().getMonth().getValue())
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .when().get(String.format(API_PATH, group.getId()))
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(DiaryMonthlyResponse.class);
+
+        assertThat(response.days()).hasSize(1);
+        DiaryDayResponse day = response.days().get(0);
+        assertThat(day.canView()).isFalse();
+        assertThat(day.day()).isEqualTo(diary.getCreatedAt().getDayOfMonth());
+        assertThat(day.profileImage()).isEqualTo(this.member.getProfileImage());
     }
 
     @Test
     void 달력형_일기_조회_실패_날짜_유효성_검사() {
         Group group = createGroup();
-        groupRepository.save(group);
 
         RestAssured
                 .given().log().all()
@@ -69,15 +95,22 @@ class DiaryViewMonthlyTest extends ApiBaseTest {
     }
 
     private Group createGroup() {
-        return Group.of("버니즈", "code");
+        return groupRepository.save(Group.of("버니즈", "code"));
+    }
+
+    private void updateSelf(Group group, LocalDate lastViewableDiaryDate) {
+        this.member.joinGroup("nickname", "/image", 1, GroupRole.GROUP_LEADER, group);
+        this.member.updateLastViewableDiaryDate(lastViewableDiaryDate);
+        memberRepository.save(member);
     }
 
     private Diary createDiary(Group group) {
-        return Diary.builder()
+        Diary diary = Diary.builder()
                 .content("내용")
                 .moodLocation("/images/write-page/emoji/sleepy.svg")
                 .group(group)
                 .member(member)
                 .build();
+        return diaryRepository.save(diary);
     }
 }
