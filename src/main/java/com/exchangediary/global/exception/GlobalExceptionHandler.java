dbca4c1a3466d6011837c6e7d803c44ceb6e0ea6
yeeuniii <anna862700@gmail.com>
2024-10-30 17:04:07 +0900

    feat: 서비스 예외 에러 로그 추가 (#337)

--- src/main/java/com/exchangediary/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/exchangediary/global/exception/GlobalExceptionHandler.java
@@ -1,13 +1,7 @@
 package com.exchangediary.global.exception;
 
-import com.exchangediary.global.exception.serviceexception.ConfilctException;
-import com.exchangediary.global.exception.serviceexception.FailedImageUploadException;
-import com.exchangediary.global.exception.serviceexception.KakaoLoginFailureException;
-import com.exchangediary.global.exception.serviceexception.DuplicateException;
 import com.exchangediary.global.exception.serviceexception.ServiceException;
-import com.exchangediary.global.exception.serviceexception.InvalidDateException;
-import com.exchangediary.global.exception.serviceexception.NotFoundException;
-import com.exchangediary.global.exception.serviceexception.UnauthorizedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,11 +13,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Objects;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ApiErrorResponse handleInvalidArgumentException(MethodArgumentNotValidException exception) {
         try {
+            log.error("{}", String.format("%s", exception.getFieldError().getDefaultMessage()));
             return ApiErrorResponse.from(Objects.requireNonNull(exception.getFieldError()));
         } catch (NullPointerException e) {
             return ApiErrorResponse.from(HttpStatus.BAD_REQUEST);
@@ -38,6 +34,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ApiErrorResponse> handleServiceException(ServiceException exception) {
+        log.error("{} caused by {}", exception.toString(), exception.getValue());
+
         ApiErrorResponse body = ApiErrorResponse.from(
                 exception.getErrorCode(),
                 exception.getMessage(),
