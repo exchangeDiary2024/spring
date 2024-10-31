f897a253fb871a0c25c2770127279d0eb6e9aa71
jisunggi <wltjd5316@naver.com>
2024-11-01 02:13:22 +0900

    feat: diaryContent 엔티티 생성 (#350)

--- src/main/java/com/exchangediary/diary/domain/entity/DiaryContent.java
+++ src/main/java/com/exchangediary/diary/domain/entity/DiaryContent.java
@@ -1,19 +1,16 @@
 package com.exchangediary.diary.domain.entity;
 
-import com.exchangediary.diary.ui.dto.request.DiaryRequest;
 import com.exchangediary.global.domain.entity.BaseEntity;
-import com.exchangediary.group.domain.entity.Group;
-import com.exchangediary.member.domain.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,8 +18,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.type.descriptor.jdbc.LongVarcharJdbcType;
-
-import java.util.List;
 
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
@@ -32,37 +27,18 @@ import static lombok.AccessLevel.PROTECTED;
 @Builder
 @NoArgsConstructor(access = PROTECTED, force = true)
 @AllArgsConstructor(access = PRIVATE)
-public class Diary extends BaseEntity {
+public class DiaryContent extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "diary_id")
+    @Column(name = "diary_content_id")
     private Long id;
+    @NotNull
+    private Integer page;
     @Lob
     @JdbcType(LongVarcharJdbcType.class)
     @NotNull
     private final String content;
-    @NotNull
-    private final String moodLocation;
-    private String imageFileName;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private final Member member;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
-    private final Group group;
-    @OneToMany(mappedBy = "diary")
-    private List<DiaryContent> contents;
-
-    public static Diary from(DiaryRequest diaryRequest, Member member, Group group) {
-        return Diary.builder()
-                .content(diaryRequest.contents())
-                .moodLocation(diaryRequest.moodLocation())
-                .member(member)
-                .group(group)
-                .build();
-    }
-
-    public void updateImageFileName(String imageFileName) {
-        this.imageFileName = imageFileName;
-    }
+    @JoinColumn(name = "dairy_id", foreignKey = @ForeignKey(name = "content_diary_id_fkey"))
+    private Diary diary;
 }
