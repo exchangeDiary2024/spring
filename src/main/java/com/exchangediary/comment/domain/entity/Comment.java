ca38e0dbefdbfcd506d28890e7689385e083c53e
jisunggi <wltjd5316@naver.com>
2024-11-06 03:32:27 +0900

    feat: 댓글 앤티티 생성 (#362)

--- src/main/java/com/exchangediary/comment/domain/entity/Comment.java
+++ src/main/java/com/exchangediary/comment/domain/entity/Comment.java
@@ -1,21 +1,18 @@
-package com.exchangediary.diary.domain.entity;
+package com.exchangediary.comment.domain.entity;
 
-import com.exchangediary.comment.domain.entity.Comment;
-import com.exchangediary.diary.ui.dto.request.DiaryRequest;
+import com.exchangediary.diary.domain.entity.Diary;
 import com.exchangediary.global.domain.entity.BaseEntity;
-import com.exchangediary.group.domain.entity.Group;
 import com.exchangediary.member.domain.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,8 +20,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.type.descriptor.jdbc.LongVarcharJdbcType;
-
-import java.util.List;
 
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
@@ -34,39 +29,25 @@ import static lombok.AccessLevel.PROTECTED;
 @Builder
 @NoArgsConstructor(access = PROTECTED, force = true)
 @AllArgsConstructor(access = PRIVATE)
-public class Diary extends BaseEntity {
+public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "diary_id")
+    @Column(name = "comment_id")
     private Long id;
+    @NotNull
+    private final Double xPosition;
+    @NotNull
+    private final Double yPosition;
     @Lob
     @JdbcType(LongVarcharJdbcType.class)
     @NotNull
-    private final String moodLocation;
-    private String imageFileName;
+    private final String content;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "diary_id", foreignKey = @ForeignKey(name = "comment_diary_id_fkey"))
+    @NotNull
+    private final Diary diary;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name = "comment_member_id_fkey"))
     @NotNull
     private final Member member;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
-    @NotNull
-    private final Group group;
-    @OneToMany(mappedBy = "diary")
-    @OrderBy("page ASC")
-    private List<DiaryContent> contents;
-    @OneToMany(mappedBy = "diary")
-    private List<Comment> comments;
-
-    public static Diary of(DiaryRequest diaryRequest, Member member, Group group) {
-        return Diary.builder()
-                .moodLocation(diaryRequest.moodLocation())
-                .member(member)
-                .group(group)
-                .build();
-    }
-
-    public void uploadImageFileName(String imageFileName) {
-        this.imageFileName = imageFileName;
-    }
 }
