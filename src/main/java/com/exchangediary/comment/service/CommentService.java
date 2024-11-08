c6a8c80ec78f3432611757598159c70dc0e52711
jisunggi <wltjd5316@naver.com>
2024-11-08 20:09:16 +0900

    refactor: CommentAuthorizationService로 분리(#362)

--- src/main/java/com/exchangediary/comment/service/CommentService.java
+++ src/main/java/com/exchangediary/comment/service/CommentService.java
@@ -7,8 +7,6 @@ import com.exchangediary.comment.ui.dto.response.CommentCreateResponse;
 import com.exchangediary.diary.domain.entity.Diary;
 import com.exchangediary.diary.service.DiaryAuthorizationService;
 import com.exchangediary.diary.service.DiaryQueryService;
-import com.exchangediary.global.exception.ErrorCode;
-import com.exchangediary.global.exception.serviceexception.ForbiddenException;
 import com.exchangediary.member.domain.entity.Member;
 import com.exchangediary.member.service.MemberQueryService;
 import lombok.RequiredArgsConstructor;
@@ -23,24 +21,16 @@ public class CommentService {
     private final DiaryQueryService diaryQueryService;
     private final DiaryAuthorizationService diaryAuthorizationService;
     private final CommentRepository commentRepository;
+    private final CommentAuthorizationService commentAuthorizationService;
 
     public CommentCreateResponse createComment(CommentCreateRequest request, Long diaryId, Long memberId) {
         Member member = memberQueryService.findMember(memberId);
         Diary diary = diaryQueryService.findDiary(diaryId);
 
         diaryAuthorizationService.checkDiaryViewable(member, diary);
-        checkCommentWritable(member, diary);
+        commentAuthorizationService.checkCommentWritable(member, diary);
         Comment comment = Comment.of(request, member, diary);
         commentRepository.save(comment);
         return CommentCreateResponse.from(comment);
-    }
-
-    public void checkCommentWritable(Member member, Diary diary) {
-        if (member == diary.getMember()) {
-            throw new ForbiddenException(ErrorCode.COMMENT_WRITE_FORBIDDEN, "", "");
-        }
-        if (commentRepository.existsByDiaryIdAndMemberId(member.getId(), diary.getId())) {
-            throw new ForbiddenException(ErrorCode.COMMENT_WRITE_FORBIDDEN, "이미 댓글을 작성하였습니다.", "");
-        }
     }
 }
