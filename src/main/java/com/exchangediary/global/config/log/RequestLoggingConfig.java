139c73492a79ed862f45d5f58ee7cda971573171
jisunggi <wltjd5316@naver.com>
2024-11-14 17:22:33 +0900

    bugfix: 일기 작성 api test 오류 해결. @Profile 추가 (#401)

--- src/main/java/com/exchangediary/global/config/log/RequestLoggingConfig.java
+++ src/main/java/com/exchangediary/global/config/log/RequestLoggingConfig.java
@@ -4,8 +4,10 @@ import com.exchangediary.global.config.log.filter.ApiLoggingFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class RequestLoggingConfig {
     @Bean
     public FilterRegistrationBean<ApiLoggingFilter> loggingFilter() {
