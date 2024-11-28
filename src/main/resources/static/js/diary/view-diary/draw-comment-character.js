b9d3fb05f50f294476fd40e0a338c1ac2b6db76e
yeeuniii <anna862700@gmail.com>
2024-11-29 00:49:45 +0900

    refactor: 기존 comment 클래스명 수정 (#341) - character로 변경

--- src/main/resources/static/js/diary/view-diary/draw-comment-character.js
+++ src/main/resources/static/js/diary/view-diary/draw-comment-character.js
@@ -13,18 +13,18 @@ function clickCommentBtn(event) {
 }
 
 function onClickCommentBtn() {
-    drawCommentProfileImage();
+    drawCommentCharacter();
     addBlur();
     commentBtn.classList.add("selected");
 }
 
 function offClickCommentBtn() {
     removeBlur();
-    commentArea.removeChild(document.querySelector(".comment"));
+    commentArea.removeChild(document.querySelector(".comment-character"));
     commentBtn.classList.remove("selected");
 }
 
-function drawCommentProfileImage() {
+function drawCommentCharacter() {
     fetch(`/api${currentPathName}/comments/verify`)
         .then(async response => {
             if (response.status === 200) {
@@ -33,18 +33,18 @@ function drawCommentProfileImage() {
             throw await response.json();
         })
         .then(data => {
-            const comment = createComment(data.profileImage);
-            commentArea.appendChild(comment);
+            const character = createCharacter(data.profileImage);
+            commentArea.appendChild(character);
         })
         .catch(data => openNotificationModal("error",  [data.message], 2000));
 }
 
-function createComment(profileImage) {
+function createCharacter(character) {
     const comment = document.createElement("div");
 
-    comment.classList.add("comment", "highlight", profileImage);
-    comment.addEventListener("touchmove", moveProfileImage);
-    comment.addEventListener("touchend", setProfileImage);
+    comment.classList.add("comment-character", "highlight", character);
+    comment.addEventListener("touchmove", moveCharacter);
+    comment.addEventListener("touchend", setCharacter);
     return comment;
 }
 
@@ -61,19 +61,19 @@ function removeBlur() {
     commentArea.classList.remove("highlight");
 }
 
-function moveProfileImage(event) {
+function moveCharacter(event) {
     event.preventDefault();
 
     const COMMENT_WIDTH = event.currentTarget.offsetWidth;
     const COMMENT_HEIGHT = event.currentTarget.offsetHeight;
-    const x = getValidProfileImageLeft(event.touches[0].clientX - PAGE_MARGIN,  COMMENT_WIDTH) - COMMENT_WIDTH / 2;
-    const y = getValidProfileImageTop(event.touches[0].clientY - COMMENT_AREA_TOP, COMMENT_HEIGHT) - COMMENT_HEIGHT / 2;
+    const x = getValidCharacterLeft(event.touches[0].clientX - PAGE_MARGIN,  COMMENT_WIDTH) - COMMENT_WIDTH / 2;
+    const y = getValidCharacterTop(event.touches[0].clientY - COMMENT_AREA_TOP, COMMENT_HEIGHT) - COMMENT_HEIGHT / 2;
 
     event.currentTarget.style.left = `${x}px`;
     event.currentTarget.style.top = `${y}px`;
 }
 
-function getValidProfileImageLeft(left, commentWidth) {
+function getValidCharacterLeft(left, commentWidth) {
     const COMMENT_AREA_WIDTH = commentArea.offsetWidth - COMMENT_AREA_BORDER * 2;
     const MINIMUM_LEFT = commentWidth / 2;
     const MAXIMUM_LEFT = MINIMUM_LEFT + COMMENT_AREA_WIDTH - commentWidth;
@@ -87,7 +87,7 @@ function getValidProfileImageLeft(left, commentWidth) {
     return left;
 }
 
-function getValidProfileImageTop(top, commentHeight) {
+function getValidCharacterTop(top, commentHeight) {
     const COMMENT_AREA_HEIGHT = commentArea.offsetHeight - COMMENT_AREA_BORDER * 2;
     const MINIMUM_TOP = commentHeight / 2;
     const MAXIMUM_TOP = MINIMUM_TOP + COMMENT_AREA_HEIGHT - commentHeight;
@@ -101,22 +101,24 @@ function getValidProfileImageTop(top, commentHeight) {
     return top;
 }
 
-function setProfileImage(event) {
+function setCharacter(event) {
     event.preventDefault();
 
-    confirmProfileImagePosition();
+    confirmCharacterPosition();
 }
 
-async function confirmProfileImagePosition() {
+async function confirmCharacterPosition() {
     const result = await openConfirmModal("여기에 댓글을 쓸까요?");
 
     if (result) {
-        const comment = document.querySelector(".comment");
+        const character = document.querySelector(".comment-character");
 
-        comment.classList.remove("highlight");
-        comment.removeEventListener("touchmove", moveProfileImage);
-        comment.removeEventListener("touchend", setProfileImage);
+        character.classList.remove("highlight");
+        character.removeEventListener("touchmove", moveCharacter);
+        character.removeEventListener("touchend", setCharacter);
         removeBlur();
         commentBtn.classList.remove("selected");
+
+        createComment();
     }
 }
