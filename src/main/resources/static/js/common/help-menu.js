f43c28d04922aff96b4375b651554e908b9f0659
jisunggi <wltjd5316@naver.com>
2024-12-03 02:36:57 +0900

    feat: 일기 도움말 버튼 구현 (#413)

--- src/main/resources/static/js/common/help-menu.js
+++ src/main/resources/static/js/common/help-menu.js
@@ -1,5 +1,9 @@
 const helpIcon = document.querySelector(".help-icon");
 const tooltip = document.querySelector(".tooltip");
+const helpButton = document.querySelector(".help-btn");
+const inquiryButton = document.querySelector(".inquiry-btn");
+const helpImageContainer = document.querySelector(".help-image-container");
+const helpImage = document.querySelector(".help-image");
 
 helpIcon.addEventListener('click', () => {
     tooltip.classList.toggle('hidden');
@@ -9,4 +13,29 @@ document.addEventListener('click', (event) => {
     if (!helpIcon.contains(event.target) && !tooltip.contains(event.target)) {
         tooltip.classList.add('hidden');
     }
+});
+
+helpButton.addEventListener('click', () => {
+    const pathname = window.location.pathname;
+    const monthlyRegexp = /^\/groups\/[a-zA-Z0-9]+$/;
+    const diaryWriteRegexp = /^\/groups\/[a-zA-Z0-9]+\/diaries$/;
+    const diaryViewRegexp = /^\/groups\/[a-zA-Z0-9]+\/diaries\/\d+$/;
+    let helpImagePath = '';
+
+    if (monthlyRegexp.test(pathname)) {
+        helpImagePath = '/images/common/help-menu/help-image/help-monthly.svg';
+    } else if (diaryWriteRegexp.test(pathname)) {
+        helpImagePath = '/images/common/help-menu/help-image/help-diary-write.svg';
+    } else if (diaryViewRegexp.test(pathname)) {
+        helpImagePath = '/images/common/help-menu/help-image/help-diary-view.svg';
+    }
+
+    helpImage.src = helpImagePath;
+    helpImageContainer.classList.remove('hidden');
+    tooltip.classList.add('hidden');
+
+});
+
+helpImage.addEventListener('click', () => {
+    helpImageContainer.classList.add('hidden');
 });
