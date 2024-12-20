6c2971767efd32493da34228fcdb0883dd8aa00d
yeeuniii <anna862700@gmail.com>
2024-12-21 03:17:15 +0900

    fix: Backspace 이벤트 처리 수정 (#341)

--- src/main/resources/static/js/diary/view-diary/comment-textarea.js
+++ src/main/resources/static/js/diary/view-diary/comment-textarea.js
@@ -3,9 +3,18 @@ function addEventInCommentTextarea() {
 
     textarea.addEventListener("keydown", (event) => {
         if (!isIME(event)) {
+            const selection = window.getSelection();
+            const range = selection.getRangeAt(0);
+            const { startContainer, startOffset } = range;
+
             if (event.key === "Backspace") {
-                removeStickerCharacter(event, textarea);
-                removeNewLine(event, textarea)
+                const isStart = startOffset === 0 && startContainer === textarea && selection.toString().length === 0;
+                if (isStart) {
+                    event.preventDefault();
+                } else {
+                    removeSticker(event, startContainer, startOffset);
+                    removeNewLine(event, startContainer);
+                }
             }
             if (event.key === "Enter") {
                 addNewLine(event, textarea)
@@ -13,24 +22,44 @@ function addEventInCommentTextarea() {
             adjustHeightByTextarea();
         }
     });
+    textarea.addEventListener("keyup", (event) => {
+        if (event.key === "Backspace") {
+            if (textarea.innerHTML === "<br>") {
+                textarea.innerHTML = "";
+            }
+        }
+    })
 }
 
 function isIME(event) {
     return event.isComposing || event.key === "Process"
 }
 
-function removeStickerCharacter(event, textarea) {
-    if (textarea.lastChild && textarea.lastChild.nodeType === Node.ELEMENT_NODE && textarea.lastElementChild.tagName === "DIV") {
-        event.preventDefault();
-        textarea.lastElementChild.remove();
+function removeSticker(event, startContainer, startOffset) {
+    if (startContainer.nodeType === Node.ELEMENT_NODE) {
+        const previousSibling = startContainer.childNodes[startOffset - 1];
+
+        if (isCursorPositionBehindSticker(previousSibling)) {
+            event.preventDefault();
+            previousSibling.remove();
+        }
     }
 }
 
-function removeNewLine(event, textarea) {
-    if (textarea.innerHTML.lastIndexOf("<br>") === textarea.innerHTML.length - 5) {
+function isCursorPositionBehindSticker(element) {
+    return element
+        && element.nodeType === Node.ELEMENT_NODE
+        && element.tagName === "DIV"
+        && element.classList.contains("sticker-character");
+}
+
+function removeNewLine(event, startContainer) {
+    if (startContainer.nodeType === Node.TEXT_NODE && startContainer.textContent === "\u200B") {
         event.preventDefault();
-        textarea.innerHTML = textarea.innerHTML.substring(0, textarea.innerHTML.length - 5);
-        moveCursorToEnd(textarea);
+        if (startContainer.previousSibling && startContainer.previousSibling.tagName === "BR") {
+            startContainer.previousSibling.remove();
+        }
+        startContainer.remove();
     }
 }
 
